0289b0fee469c3049d8ef017d65e28adf785049f
Ewen AK <ewenak@gmx.com>
2019-02-02 20:12:10 +0100

    ouzhpenn ar c'herioù nevez

--- geriou/geriou.js
+++ geriou/geriou.js
@@ -11,33 +11,49 @@ function print(s, klas) {
     enank.scrollIntoView();
 }
 
-fetch('holl_geriou.txt').then(r => r.text().then(geriou => {
-    geriou = new Map(geriou.split('\n').filter(s => s.includes(':')).map(s => s.split(': ')));
-    var n_geriou_mat = 0;
-    var enank = document.getElementById('enank');
-    var enankou = Array.from(geriou.entries());
-    var ger = enankou[Math.floor(Math.random() * enankou.length)];
-    print(ger[0], 'urzh'); //urzh evit urzhiataer
+print("C'hoant peus deskiñ 1) an holl gerioù pe 2) nemet ar re nevez ?", 'urzh');
 
-    function ger_f(talvoud) {
-        if (talvoud.toLowerCase() === ger[1].toLowerCase()) {
-            n_geriou_mat = n_geriou_mat + 1;
-            print(talvoud, 'den');
-            print('Mat !', 'urzh');
-        } else {
-            print(talvoud, 'den');
-            print('Ket ! ' + ger[1] + ' eo.', "urzh");
-        }
-        ger = enankou[Math.floor(Math.random() * enankou.length)];
-        print(ger[0], 'urzh');
-    }
+function get_data(url) {
+    return fetch(url).then(r => r.text().then(geriou => 
+        new Map(geriou.split('\n').filter(s => s.includes(':')).map(s => s.split(': ')))));
+}
 
-    enank.addEventListener('keyup', e => {
-        if (e.key === 'Enter') {
-            var talvoud = enank.value;
-            enank.value = '';
-            ger_f(talvoud);
-        };
-    });
-})
-);
+Promise.all( [ get_data('geriou_nevez.txt'),
+               get_data('holl_geriou.txt') ]).then( ([geriou_nevez, holl_geriou]) => {
+                var geriou
+                var enankou
+                var ger
+                var n_geriou_mat = 0;
+                var enank = document.getElementById('enank');
+                var holl_pe_nevez
+
+                function ger_f(talvoud) {
+                    if (talvoud.toLowerCase() === ger[1].toLowerCase()) {
+                        n_geriou_mat = n_geriou_mat + 1;
+                        print(talvoud, 'den');
+                        print('Mat !', 'urzh');
+                    } else {
+                        print(talvoud, 'den');
+                        print('Ket ! ' + ger[1] + ' eo.', "urzh");
+                    }
+                    ger = enankou[Math.floor(Math.random() * enankou.length)];
+                    print(ger[0], 'urzh');
+                }
+
+                enank.addEventListener('keyup', e => {
+                    if (e.key === 'Enter') {
+                        if (typeof holl_pe_nevez === "undefined"){
+                            holl_pe_nevez = enank.value === '1' ? 'holl' : 'nevez';
+                            print(enank.value, 'den');
+                            enank.value = '';
+                            geriou = new Map(holl_pe_nevez === "holl" ? [...geriou_nevez, ...holl_geriou] : geriou_nevez);
+                            enankou = Array.from(geriou.entries());
+                            ger = enankou[Math.floor(Math.random() * enankou.length)];
+                            print(ger[0], 'urzh'); //urzh evit urzhiataer
+                        } else {
+                            var talvoud = enank.value;
+                            enank.value = '';
+                            ger_f(talvoud);
+                    };
+                }});
+});
